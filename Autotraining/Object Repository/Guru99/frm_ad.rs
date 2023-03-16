767b9ab1cd23c7206622618ac6961862a4f383c5
anhkieu <kieuanh2512@gmail.com>
2023-03-16 16:49:01 +0700

    update tc05

---
+++ Autotraining/Object Repository/Guru99/frm_ad.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>frm_ad</name>
+   <tag></tag>
+   <elementGuidId>ae52fcd7-4da4-44b2-9545-45a6076e9ce0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//iframe[contains(@id,'google_ads_iframe')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
