74b9a51200c2a4d0184d6d342f620027c1425aa9
anhkieu <kieuanh2512@gmail.com>
2023-03-16 16:14:04 +0700

    Update tc05

--- Autotraining/Scripts/Topic 9-10/TC05_Practice_Demoguru99_Verifymessage/Script1678787745684.groovy
+++ Autotraining/Scripts/Topic 9-10/TC05_Practice_Demoguru99_Verifymessage/Script1678787745684.groovy
@@ -1,9 +1,13 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+
 
 'Step 1 Open "https://demo.guru99.com/v4/"'
 WebUI.openBrowser('https://demo.guru99.com/v4/')
+//maximize page
+//WebUI.maximizeWindow()
 
 //WebUI.refresh()
 'Step 2: Click "here" link at the bottom'
@@ -11,51 +15,55 @@ WebUI.click(findTestObject('Object Repository/Guru99/lnk_here'))
 
 'Step 3: Input Email ID'
 //delay
-WebUI.delay(3)
-//refresh page to ignore ad
-WebUI.refresh()
-//navigate to home page
-WebUI.navigateToUrl('https://demo.guru99.com/v4/')
-//click here again
-WebUI.click(findTestObject('Object Repository/Guru99/lnk_here'))
-//wait email ID visible
-WebUI.waitForElementVisible(findTestObject('Object Repository/Guru99/txt_emailid'), 0)
-//input email ID
-WebUI.setText(findTestObject('Object Repository/Guru99/txt_emailid'), 'katest@mail.com')
+WebUI.delay(5)
 
-'Step 4: Click Submit button'
-WebUI.click(findTestObject('Object Repository/Guru99/btn_submit'))
+//check ad present
+if(WebUI.verifyElementPresent(findTestObject('Guru99/frm_ad'),3, FailureHandling.OPTIONAL)==true){
+	//switch to ad iframe
+	WebUI.switchToFrame(findTestObject('Guru99/frm_ad'), 0)
+	//WebUI.doubleClick(findTestObject('Guru99/frm_ad'))
+	//close the ad
+	WebUI.click(findTestObject('Object Repository/Guru99/btn_closead'))
 
-//get user id
-'Step 5: Get User ID and password'
-String userid = WebUI.getText(findTestObject('Guru99/txa_userid'))
-
-//get password
-String password = WebUI.getText(findTestObject('Guru99/txa_password'))
-
-'Step 6: Navigate to https://demo.guru99.com/v4/ again'
-WebUI.navigateToUrl('https://demo.guru99.com/v4/')
-
-//maximize page
-WebUI.maximizeWindow()
-
-//input User ID
-'Step 7: Input User ID and password at step 5'
-WebUI.setText(findTestObject('Object Repository/Guru99/txt_userid'), userid)
-
-//input password
-WebUI.setText(findTestObject('Object Repository/Guru99/txt_password'), password)
-
-'Step 8: Click Login button'
-WebUI.click(findTestObject('Object Repository/Guru99/btn_login'))
-
-'Step 9: Verify message: Welcome to Managers Page of Guru99 Bank displayed'
-String welcome = WebUI.getText(findTestObject('Object Repository/Guru99/txa_welcome'))
-
-WebUI.verifyEqual(welcome, 'Welcome To Manager\'s Page of Guru99 Bank')
-
-'Step 10: Close browser'
-WebUI.delay(3)
-
-WebUI.closeBrowser()
-
+}
+  //Switch to default page
+	WebUI.switchToDefaultContent()
+	
+	//input email ID
+	WebUI.setText(findTestObject('Object Repository/Guru99/txt_emailid'), 'katest@mail.com')
+	
+	'Step 4: Click Submit button'
+	WebUI.click(findTestObject('Object Repository/Guru99/btn_submit'))
+	
+	//get user id
+	'Step 5: Get User ID and password'
+	String userid = WebUI.getText(findTestObject('Guru99/txa_userid'))
+	
+	//get password
+	String password = WebUI.getText(findTestObject('Guru99/txa_password'))
+	
+	'Step 6: Navigate to https://demo.guru99.com/v4/ again'
+	WebUI.navigateToUrl('https://demo.guru99.com/v4/')
+	
+	//maximize page
+	WebUI.maximizeWindow()
+	
+	//input User ID
+	'Step 7: Input User ID and password at step 5'
+	WebUI.setText(findTestObject('Object Repository/Guru99/txt_userid'), userid)
+	
+	//input password
+	WebUI.setText(findTestObject('Object Repository/Guru99/txt_password'), password)
+	
+	'Step 8: Click Login button'
+	WebUI.click(findTestObject('Object Repository/Guru99/btn_login'))
+	
+	'Step 9: Verify message: Welcome to Managers Page of Guru99 Bank displayed'
+	String welcome = WebUI.getText(findTestObject('Object Repository/Guru99/txa_welcome'))
+	
+	WebUI.verifyEqual(welcome, 'Welcome To Manager\'s Page of Guru99 Bank')
+	
+	'Step 10: Close browser'
+	WebUI.delay(3)
+	
+	WebUI.closeBrowser()
